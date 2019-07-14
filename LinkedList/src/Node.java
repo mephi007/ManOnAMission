855efdf7ddaf3d0e8d66f1c776e12d27fa9e943d
mephi007 <roysumit949@gmail.com>
2019-07-14 20:21:14 +0530

    created Node Class

---
+++ LinkedList/src/Node.java
@@ -0,0 +1,32 @@
+
+public class Node {
+		private int data;
+		private Node next;
+		
+		public Node()
+		{
+			this.data = 0;
+			this.next = null;
+		}
+		
+		public Node(int d, Node n)
+		{
+			this.data = d;
+			this.next = n;
+		}
+		
+		public void setData(int val)
+		{
+			this.data = val;
+		}
+		
+		public void setNext(Node n)
+		{
+			this.next = n;
+		}
+		
+		public int getData()
+		{
+			return this.data;
+		}
+}
