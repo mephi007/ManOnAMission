bb1cae9e02767d9a82cfad5438d66fb831075b9e
mephi007 <roysumit949@gmail.com>
2019-07-14 21:57:40 +0530

    Driver method performing all the insertion and deletion operation

---
+++ LinkedList/src/ListDriver.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ListDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner in = new Scanner(System.in);
+			List list = new List();
+			
+			list.insertAtFirst(10);
+			list.insertAtFirst(20);
+			list.insertAtLast(30);
+			list.insertAtPos(40, 2);
+			list.viewList();
+			System.out.println("----------------");
+			list.deleteAtFirst();
+			list.deleteAtEnd();
+			list.deleteAtPos(2);
+			list.viewList();
+
+	}
+
+}
