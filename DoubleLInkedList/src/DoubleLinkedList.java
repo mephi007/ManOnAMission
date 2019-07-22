202d6c1bbadcfea79b0ea278394b461c219d427b
mephi007 <roysumit949@gmail.com>
2019-07-22 09:13:48 +0530

    method to check size of the list

--- DoubleLInkedList/src/DoubleLinkedList.java
+++ DoubleLInkedList/src/DoubleLinkedList.java
@@ -15,5 +15,26 @@ public class DoubleLinkedList {
 		}
 		return false;
 	}
+	
+	//method to check the size of the list
+	public int Size()
+	{
+		int count = 0;
+		if(start == null)
+			count =0;
+		else {
+			count = 1;
+			Node t = start;
+			while(t.getNext() != null)
+			{
+				t = t.getNext();
+				count++;
+			}
+		}
+		return count;
+	}
+	
+//	//method to insert node at first
+//	public void insertAtFirst(int data)
 
 }
---
+++ DoubleLInkedList/src/DoubleLinkedListDriver.java
@@ -0,0 +1,13 @@
+
+public class DoubleLinkedListDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DoubleLinkedList dlist = new DoubleLinkedList();
+		
+		dlist.isEmpty();
+		System.out.println(dlist.Size());
+
+	}
+
+}
