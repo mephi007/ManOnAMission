fc04d8a83746943a1e42bfdb82e6f9adb361659d
mephi007 <roysumit949@gmail.com>
2019-07-22 09:09:12 +0530

    method to check if the list is empty or not

--- DoubleLInkedList/src/DoubleLinkedList.java
+++ DoubleLInkedList/src/DoubleLinkedList.java
@@ -1,4 +1,19 @@
 public class DoubleLinkedList {
 	Node start;
+	
+	public DoubleLinkedList()
+	{
+		this.start = null;
+	}
+	
+	//to check if the list if empty
+	public boolean isEmpty() {
+		if(start == null)
+		{
+			System.out.println("list is empty");
+			return true;
+		}
+		return false;
+	}
 
 }
