2a4d9b1c62f8227b026f84c3869bdcce396afd89
mephi007 <roysumit949@gmail.com>
2019-07-24 20:49:23 +0530

    method to check whether the list is empty or not

--- CircularList/src/CircularList.java
+++ CircularList/src/CircularList.java
@@ -7,5 +7,16 @@ public class CircularList {
 	{
 		this.start = null;
 	}
+	
+	//check if list is empty
+	public boolean isEmpty()
+	{
+		boolean flag = false;
+		if(this.start == null) {
+			System.out.println("list is empty");
+			flag = true;
+			}
+		return flag;			
+	}
 
 }
