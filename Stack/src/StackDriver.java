d8f979ee01d863ef7409598fa93dc4ba433c0baf
mephi007 <roysumit949@gmail.com>
2019-07-29 11:09:56 +0530

    method to push element into stack and overflow method

--- Stack/src/StackDriver.java
+++ Stack/src/StackDriver.java
@@ -8,8 +8,22 @@ public class StackDriver {
 		stackArr stck = new stackArr(5);
 		stck.isEmpty();
 		stck.viewStack();
+		System.out.println("-----------pushing 1 into stack---------");
+		stck.push(1);
+		stck.viewStack();
+		System.out.println("-----------pushing 2 into stack---------");
+		stck.push(2);
+		stck.viewStack();
+		System.out.println("-----------pushing 3 into stack---------");
+		stck.push(3);
+		stck.viewStack();
+		System.out.println("-----------pushing 4 into stack---------");
+		stck.push(4);
+		stck.viewStack();
+		System.out.println("-----------pushing 5 into stack---------");
+		stck.push(5);
+		//stck.viewStack();
 		
-
 	}
 
 }
--- Stack/src/stackArr.java
+++ Stack/src/stackArr.java
@@ -12,10 +12,11 @@ public class stackArr {
 		 top = 0;
 	}
 	
+	//to check whether the stack is empty or not
 	public boolean isEmpty()
 	{
 		boolean flag = false;
-		if(top <= 0)
+		if(top == 0)
 		{
 			System.out.println("Stack is empty");
 			flag = true;
@@ -24,16 +25,43 @@ public class stackArr {
 		return flag;
 	}
 	
+	//to view all the element of the stack
 	public void viewStack()
 	{
 		if(isEmpty());
 		else {
-			for(int i= top; i>=0; i--)
+			for(int i= top; i>0; i--)
 			{
-				System.out.print(arr[i]);
+				System.out.println(arr[i]);
 			}
 		}
 	}
+	
+	//to check whether the stack is OverFlow/full
+	public boolean overFlow()
+	{
+		boolean flag = false;
+		if(top == size-1)
+		{
+			System.out.println("Stack OverFlow");
+			flag = true;
+		}
+		return flag;
+	}
+	
+	//to push/insert element into the stack
+	public void push(int data)
+	{
+		if(overFlow()) {
+			System.out.println("Sorry, cannot push element into stack");
+		}
+		else {
+			top = top+1;
+			arr[top] = data;
+		}
+	}
+	
+	
 	
 	
 
