1b2da7321619eaf2e1c6ce0d2c55e1a98d4c515b
mephi007 <roysumit949@gmail.com>
2019-07-29 11:45:31 +0530

    method to check underflow condition

--- Queue/src/queueArr.java
+++ Queue/src/queueArr.java
@@ -1,3 +1,22 @@
 public class queueArr {
 
+	private int front, rear, size;
+	private int[] arr;
+
+	public queueArr(int s) {
+		// TODO Auto-generated constructor stub
+		this.size = s;
+		arr = new int[size];
+		front = rear = 0;
+	}
+
+	// underFlow Condition i.e whether the queue is empty or not
+	public boolean isEmpty() {
+		boolean flag = false;
+		if (front == 0 && rear == 0) {
+			System.out.println("Queue is empty");
+			flag = true;
+		}
+		return flag;
+	}
 }
