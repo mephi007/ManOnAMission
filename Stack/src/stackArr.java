dff235337441de102a4d8f2b0317f67f763937f1
mephi007 <roysumit949@gmail.com>
2019-07-29 10:46:48 +0530

    starting Stack

---
+++ Stack/src/stackArr.java
@@ -0,0 +1,15 @@
+
+public class stackArr {
+	
+	private int top;
+	private int size;
+	private  int arr[];
+	
+	public stackArr(int s) {
+		// TODO Auto-generated constructor stub
+		this.size = s;
+		 arr = new int[this.size];
+		
+	}
+
+}
