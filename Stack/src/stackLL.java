362d4a1114e9c10b58724a48023b60be857c705a
mephi007 <roysumit949@gmail.com>
2019-07-30 23:49:38 +0530

    method push, isEmpty, peek

--- Stack/src/stackLL.java
+++ Stack/src/stackLL.java
@@ -1,11 +1,34 @@
 
 public class stackLL {
-	
+
 	stackNode top;
-	
-	public stackLL()
-	{
+
+	public stackLL() {
 		top = null;
+	}
+
+	// empty or not
+	public boolean isEmpty() {
+		boolean flag = false;
+		if (top == null) {
+			System.out.println("the stack is empty");
+			flag = true;
+		}
+		return flag;
+	}
+
+	// push
+	public void push(int x) {
+		stackNode n = new stackNode();
+		n.setData(x);
+		n.setNext(top);
+		top = n;
+	}
+	
+	//peek
+	public int peek()
+	{
+		return top.getData();
 	}
 
 }
---
+++ Stack/src/stackLLDriver.java
@@ -0,0 +1,14 @@
+
+public class stackLLDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		stackLL s = new stackLL();
+		s.isEmpty();
+		s.push(1);
+		System.out.println(s.peek());
+
+	}
+
+}
