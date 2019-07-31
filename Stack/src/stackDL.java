9f7064774647338a1afeff19e6de85cd23b01ef4
mephi007 <roysumit949@gmail.com>
2019-07-31 10:51:42 +0530

    delete middle node

--- Stack/src/stackDL.java
+++ Stack/src/stackDL.java
@@ -1,70 +1,96 @@
 
 public class stackDL {
-	
+
 	stackDLNode top;
+	stackDLNode mid;
 	static int size;
-	
+
 	public stackDL() {
 		top = null;
-		size=0;
+		mid = null;
+		size = 0;
 	}
-	
-	//empty 
-	public boolean isEmpty()
-	{
+
+	// empty
+	public boolean isEmpty() {
 		boolean flag = false;
-		if(top == null)
-		{
+		if (top == null) {
 			System.out.println("stack is empty");
 			flag = true;
 		}
-		
+
 		return flag;
 	}
-	
-	//push
-	public void push(int x)
-	{
+
+	// push
+	public void push(int x) {
 		stackDLNode n = new stackDLNode();
 		n.setData(x);
-		if(isEmpty()) {
+		if (isEmpty()) {
 			top = n;
-		}
-		else {
-		stackDLNode temp = top;
-		top = n;
-		n.setNext(temp);
-		temp.setPrev(n);
+		} else {
+			stackDLNode temp = top;
+			top = n;
+			n.setNext(temp);
+			temp.setPrev(n);
 		}
 		size++;
+		if (size == 1) {
+			mid = n;
+		} else {
+			if (size % 2 == 0) {
+				mid = mid.getPrev();
+			}
+		}
 	}
-	
-	//size
-	public int Size()
-	{
+
+	// size
+	public int Size() {
 		return size;
 	}
-	
-	//peek
-	public void peek()
-	{
-		if(isEmpty());
+
+	// peek
+	public void peek() {
+		if (isEmpty())
+			;
 		else
-		System.out.println(top.getData());
+			System.out.println(top.getData());
 	}
-	
-	//pop
-	public void pop()
-	{
-		if(size == 1)
-		{
+
+	// pop
+	public void pop() {
+		if (size == 1) {
 			top = null;
-		}
-		else {
+		} else {
 			top = top.getNext();
 			top.setPrev(null);
 		}
 		size--;
+	}
+
+	// FindMiddle
+	public stackDLNode middle() {
+		if (top == null) {
+			System.out.println("stack is already empty");
+			return null;
+		} else {
+			return mid;
+		}
+	}
+
+	// delete middle
+	public void deleteMid() {
+		if (size == 1 || size == 2) {
+			top = null;
+			size--;
+		} else if (size == 0)
+			return;
+		else {
+			stackDLNode temp = mid.getPrev();
+			temp.setNext(mid.getNext());
+			mid.getNext().setNext(temp);
+			size--;
+		}
 	}
 
 }
--- Stack/src/stackDLDriver.java
+++ Stack/src/stackDLDriver.java
@@ -5,17 +5,20 @@ public class stackDLDriver {
 		// TODO Auto-generated method stub
 		stackDL s = new stackDL();
 		s.isEmpty();
+		System.out.println("----------pushing----------");
 		s.push(1);
 		s.peek();
-		s.pop();
 		s.push(2);
 		s.peek();
 		s.push(3);
 		s.peek();
-		System.out.println(s.Size());
-		s.pop();
-		s.peek();
-		System.out.println(s.Size());
+		System.out.println("Size of the stack \n" +s.Size());
+		System.out.println("Middle node of the stack \n"+ s.middle().getData());
+		System.out.println("----deleting middle node----");
+		s.deleteMid();
+		System.out.println("Size of the stack \n" +s.Size());
+		
+
 	}
 
 }
