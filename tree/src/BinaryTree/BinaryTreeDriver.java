4fdc96a1e63ccbab4e6c59e8395d97c5a66010a4
mephi007 <roysumit949@gmail.com>
2019-10-01 01:07:06 +0530

    last work got wrecked, submitting again

---
+++ tree/src/BinaryTree/BinaryTreeDriver.java
@@ -0,0 +1,137 @@
+package BinaryTree;
+
+public class BinaryTreeDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Operation bt = new Operation();
+		
+		bt.add(6);
+		bt.add(4);
+		bt.add(8);
+		bt.add(3);
+		bt.add(5);
+		bt.add(7);
+		bt.add(9);
+		System.out.println(bt.root.getData());
+		System.out.println("-----------In Order Traversing---------------");
+		bt.InOrder(bt.root);
+		System.out.println("-----------Pre Order Traversing---------------");
+		bt.PreOrder(bt.root);
+		System.out.println("-----------Post Order Traversing---------------");
+		bt.PostOrder(bt.root);
+		System.out.println("-----------Level Order Traversing---------------");
+		bt.LevelOrder();
+		System.out.println("--------------------------------------");
+		Operation bt1 = new Operation();
+		bt1.insert(5);
+		bt1.insert(8);
+		bt1.insert(9);
+		bt1.LevelOrder();
+		System.out.println("---------right most node-----------");
+		System.out.println(bt.rightMost(bt.root).getData());
+		System.out.println("----------delete root-----------");
+		bt.deleteNode(5);
+		bt.LevelOrder();
+		System.out.println("---------Printing leaf of the Tree------------");
+		bt.printLeaf(bt.root);
+		System.out.println("---------------");
+		bt1.printLeaf(bt1.root);
+		System.out.println("-----printing leaf--------");
+		bt.printKDist(bt.root, 1);
+//		System.out.println("-------continous tree or not----------");
+//		bt.contTree();
+		System.out.println("----------Iterative InOrder------------");
+		bt.IterativeInOrder();
+		System.out.println("----------Iterative PreOrder------------");
+		bt.IterativePreOrder();
+		System.out.println("----------Iterative PostOrder------------");
+		bt.IterativePostOrder();
+		
+		System.out.println("-------continous tree or not----------");
+		System.out.println(bt.contTree(bt.root));
+		Operation bt2 = new Operation();
+		bt2.add(3);
+		bt2.add(2);
+		bt2.add(4);
+		bt2.add(1);
+		bt2.add(3);
+		bt2.add(5);
+		System.out.println("-----------Level Order Traversing---------------");
+		bt2.LevelOrder();
+		System.out.println("-------continous tree or not----------");
+		System.out.println(bt2.contTree(bt2.root));
+		
+		Operation bt3 = new Operation();
+		bt3.insert(1);
+		bt3.insert(2);
+		bt3.insert(3);
+		bt3.insert(4);
+		bt3.insert(5);
+		System.out.println("-----------tree before mirror----------");
+		bt3.InOrder(bt3.root);
+		System.out.println("---------leftView-----------");
+		bt3.leftView();
+		bt3.mirror(bt3.root);
+		System.out.println("-----------tree after mirror----------");
+		bt3.InOrder(bt3.root);
+		
+		Operation bt4 = new Operation();
+		bt4.add(10);
+		bt4.add(7);
+		bt4.add(15);
+		bt4.add(5);
+		bt4.add(11);
+		System.out.println("------------------------");
+		bt3.InOrder(bt4.root);
+		System.out.println("is it foldable::::" + bt4.isFoldable(bt4.root));
+		System.out.println("---------leftView-----------");
+		bt3.leftView();
+		System.out.println("------------new tree-------------");
+		Operation bt5 = new Operation();
+		bt5.insert(1);
+		bt5.insert(2);
+		bt5.insert(3);
+		bt5.insert(7);
+		bt5.insert(6);
+		bt5.insert(5);
+		bt5.insert(4);
+		bt5.LevelOrder();
+		System.out.println("Level order traversal with direction change after every two levels");
+		bt5.modifiedLevelOrder(bt5.root);
+		System.out.println("Reversal Level Order traversal");
+		bt5.RevLevelOrder();
+		System.out.println("---inOrder------");
+		bt5.InOrder(bt5.root);
+		System.out.println("---reverse tree path---");
+		bt5.reversePath(bt5.root, 7);
+		bt5.InOrder(bt5.root);
+		Operation bt6 = new Operation();
+		bt6.insert(1);
+		bt6.insert(2);
+		bt6.insert(3);
+		bt6.insert(7);
+		bt6.insert(6);
+		bt6.insert(5);
+		bt6.insert(4);
+		System.out.println("new tree---------");
+		bt6.LevelOrder();
+		System.out.println("-----Specific Level order Traversal-----");
+		bt6.specificLevelOrder(bt6.root);
+		System.out.println("-------Lowest Common Ancestor---------");
+		Operation low = new Operation();
+		low.insert(1);
+		low.insert(2);
+		low.insert(3);
+		low.insert(4);
+		low.insert(5);
+		low.insert(6);
+		low.insert(7);
+		low.PostOrder(low.root);
+		Node lcaRes = low.lca(low.root, 4, 5);
+		System.out.println();
+		System.out.println(lcaRes.getData());
+
+}
+}
---
+++ tree/src/BinaryTree/Operation.java
@@ -0,0 +1,526 @@
+package BinaryTree;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Operation {
+	Node root;
+
+	private Node addRec(Node cur, int value) {
+		if (cur == null) {
+			return new Node(value);
+		}
+		if (value < cur.getData())
+			cur.setLeft(addRec(cur.getLeft(), value));
+		else if (value > cur.getData())
+			cur.setRight(addRec(cur.getRight(), value));
+		else
+			return cur;
+
+		return cur;
+	}
+
+	public void add(int value) {
+		root = addRec(root, value);
+	}
+
+	// Depth-First Traversing
+
+	// In-Order Traversing(Left, Root, Right)
+	public void InOrder(Node n) {
+		if (n != null) {
+			InOrder(n.getLeft());
+			System.out.println(n.getData());
+			InOrder(n.getRight());
+		}
+	}
+
+	// Pre-Order Traversing(Root, Left, Right)
+	public void PreOrder(Node n) {
+		if (n != null) {
+			System.out.println(n.getData());
+			PreOrder(n.getLeft());
+			PreOrder(n.getRight());
+		}
+	}
+
+	// Post-Order Traversing(Left, Right, Root)
+	public void PostOrder(Node n) {
+		if (n != null) {
+			PostOrder(n.getLeft());
+			PostOrder(n.getRight());
+			System.out.println(n.getData());
+		}
+	}
+
+	// Breadth-First Traversal
+	public void LevelOrder() {
+		if (root == null)
+			return;
+
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(root);
+		while (!q.isEmpty()) {
+			Node n = q.remove();
+			System.out.println(n.getData());
+			if (n.getLeft() != null) {
+				q.add(n.getLeft());
+			}
+			if (n.getRight() != null) {
+				q.add(n.getRight());
+			}
+		}
+	}
+
+	// LevelOrder Traversal Insertion
+	public void insert(int value) {
+		if (root == null) {
+			root = new Node(value);
+		} else {
+			Queue<Node> que = new LinkedList<Node>();
+			que.add(root);
+
+			while (!que.isEmpty()) {
+				Node temp = que.remove();
+				if (temp.getLeft() == null) {
+					temp.setLeft(new Node(value));
+					break;
+				} else
+					que.add(temp.getLeft());
+				if (temp.getRight() == null) {
+					temp.setRight(new Node(value));
+					break;
+				} else
+					que.add(temp.getRight());
+			}
+		}
+	}
+
+	// find the rightMost Node
+	public Node rightMost(Node r) {
+		Queue<Node> que = new LinkedList<Node>();
+		que.add(r);
+		while (!que.isEmpty()) {
+			Node temp = que.remove();
+			if (temp.getRight().getRight() == null) {
+//				temp.setRight(null);
+				return temp;
+			} else
+				que.add(temp.getRight());
+		}
+		return null;
+	}
+
+	// delete node
+	public void deleteNode(int val) {
+		Queue<Node> que = new LinkedList<Node>();
+		Node temp = null;
+		Node left = null;
+		Node right = null;
+		Node replacingNode = rightMost(this.root);
+
+		if (root == null)
+			return;
+
+		if (root.getData() == val) {
+			left = root.getLeft();
+			right = root.getRight();
+			replacingNode.getRight().setLeft(left);
+			replacingNode.getRight().setRight(right);
+			this.root = replacingNode.getRight();
+			replacingNode.setRight(null);
+		} else
+			que.add(root);
+
+		while (!que.isEmpty()) {
+			temp = que.remove();
+			if (temp.getLeft() != null) {
+				if (temp.getLeft().getData() == val) {
+					left = temp.getLeft().getLeft();
+					right = temp.getLeft().getRight();
+					replacingNode.getRight().setLeft(left);
+					replacingNode.getRight().setRight(right);
+					temp.setLeft(replacingNode.getRight());
+					replacingNode.setRight(null);
+					break;
+				}
+			} else
+				que.add(temp.getLeft());
+			if (temp.getRight() != null) {
+				if (temp.getRight().getData() == val) {
+					left = temp.getRight().getLeft();
+					if (temp.getRight().getRight().getData() != replacingNode.getRight().getData()) {
+						right = temp.getRight().getRight();
+					}
+					replacingNode.getRight().setLeft(left);
+					replacingNode.getRight().setRight(right);
+					temp.setRight(replacingNode.getRight());
+					replacingNode.setRight(null);
+					break;
+				}
+			} else
+				que.add(temp.getRight());
+
+			delLeaf(temp, val);
+
+		}
+	}
+
+	private Node delLeaf(Node cur, int value) {
+		if (cur == null)
+			return null;
+
+		cur.setLeft(delLeaf(cur.getLeft(), value));
+		cur.setRight(delLeaf(cur.getRight(), value));
+
+		if (cur.getLeft() == null && cur.getRight() == null && cur.getData() == value)
+			return null;
+
+		return cur;
+	}
+
+	public void printLeaf(Node cur) {
+		if (cur == null)
+			return;
+
+		printLeaf(cur.getLeft());
+		printLeaf(cur.getRight());
+
+		if (cur.getLeft() == null && cur.getRight() == null)
+			System.out.println(cur.getData());
+
+	}
+
+	public void printKDist(Node n, int k) {
+		if (n == null) {
+			return;
+		}
+		if (k == 0) {
+			System.out.println(n.getData());
+			return;
+		} else {
+			printKDist(n.getLeft(), k - 1);
+			printKDist(n.getRight(), k - 1);
+		}
+	}
+
+	public void IterativeInOrder() {
+		Node p = root;
+		Stack<Node> st = new Stack<Node>();
+		while (true) {
+			while (p != null) {
+				st.push(p);
+				p = p.getLeft();
+			}
+			if (st.isEmpty())
+				break;
+
+			p = st.pop();
+			System.out.println(p.getData());
+			p = p.getRight();
+		}
+	}
+
+	public void IterativePreOrder() {
+		Node p = root;
+		Stack<Node> st = new Stack<Node>();
+		while (true) {
+			while (p != null) {
+				System.out.println(p.getData());
+				st.push(p);
+				p = p.getLeft();
+			}
+			if (st.isEmpty())
+				break;
+
+			p = st.pop();
+			p = p.getRight();
+		}
+	}
+
+	public void IterativePostOrder() {
+		Node p = root;
+		Stack<Node> st = new Stack<Node>();
+		while (true) {
+			if (p != null) {
+				st.push(p);
+				p = p.getLeft();
+			} else {
+				if (st.isEmpty()) {
+					break;
+				} else {
+					if (st.peek().getRight() == null) {
+						p = st.pop();
+						System.out.println(p.getData());
+						while (p == st.peek().getRight()) {
+							System.out.println(st.peek().getData());
+							p = st.pop();
+							if(st.isEmpty())
+								break;
+						}
+					}
+					if (!st.isEmpty())
+						p = st.peek().getRight();
+					else
+						p = null;
+				}
+			}
+		}
+	}
+	
+	public boolean contTree(Node cur) {
+		if(cur == null) {
+			return true;
+		}
+		
+		if(cur.getLeft() == null && cur.getRight() == null) {
+			return true;
+		}
+		
+		if(cur.getLeft() == null) {
+			return (Math.abs(cur.getData() - cur.getRight().getData()) == 1 && contTree(cur.getRight()));
+		}
+		
+		if(cur.getRight() == null) {
+			return (Math.abs(cur.getData() - cur.getLeft().getData()) == 1 && contTree(cur.getLeft()));
+		}
+		
+		return (Math.abs(cur.getData() - cur.getRight().getData()) == 1 && contTree(cur.getRight())) && 
+				(Math.abs(cur.getData() - cur.getLeft().getData()) == 1 && contTree(cur.getLeft()));
+	
+	}
+	
+	public Node mirror(Node n) {
+		if(n == null) {
+			return n;
+		}
+		
+		Node left = mirror(n.getLeft());
+		Node right = mirror(n.getRight());
+		
+		n.setLeft(right);
+		n.setRight(left);
+		
+		return n;
+	}
+	
+	private boolean isStructSame(Node a, Node b) {
+		if( a == null && b == null) {
+			return true;
+		}
+		
+		if( a != null && b != null && isStructSame(a.getLeft(), b.getLeft()) && isStructSame(a.getRight(), b.getRight())) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean isFoldable(Node n)
+	{
+		boolean res;
+		
+		if(n == null)
+			return true;
+		
+		mirror(n.getLeft());
+		
+		res = isStructSame(n.getLeft(), n.getRight());
+		
+		mirror(n.getRight());
+		
+		return res;
+	}
+	
+	//leftView
+	
+	public void leftView() {
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(root);
+		q.add(null);
+		while(!q.isEmpty()) {
+			Node temp = q.poll();
+			if(temp != null) {
+				System.out.println(temp.getData());
+				while(temp != null) {
+					if(temp.getLeft() != null) {
+						q.add(temp.getLeft());
+					}
+					if(temp.getRight() != null) {
+						q.add(temp.getRight());
+					}
+					temp = q.poll();
+				}
+				q.add(null);
+			}
+		}
+	}
+	
+	//Level order traversal with direction change after every two levels
+	public void modifiedLevelOrder(Node n) {
+		if(n == null) {
+			return;
+		}
+		if(n.getLeft() == null && n.getRight() == null) {
+			System.out.println(n.getData());
+			return;
+		}
+		
+		Queue<Node> q = new LinkedList<Node>();
+		Stack<Node> s = new Stack<Node>();
+		int size;
+		int ct =0;
+		Node temp = null;
+		boolean rightToLeft = false;
+		q.add(n);
+		while(!q.isEmpty()) {
+			ct++;
+			size = q.size();
+			for(int i=0; i< size; i++) {
+				temp = q.poll();
+				if(rightToLeft == false) {
+					System.out.print(temp.getData()+" ");
+				}
+				else {
+					s.push(temp);
+				}
+				if(temp.getLeft() != null)
+					q.add(temp.getLeft());
+				if(temp.getRight() != null)
+					q.add(temp.getRight());
+			}
+			if(rightToLeft == true) {
+				while(!s.isEmpty()) {
+					temp = s.pop();
+					System.out.print(temp.getData()+" ");
+				}
+			}
+			if(ct == 2) {
+				rightToLeft = !rightToLeft;
+				ct = 0;
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	//Reverse Level Order
+	public void RevLevelOrder() {
+		Queue<Node> q = new LinkedList<Node>();
+		Stack<Node> s = new Stack<Node>();
+		Node temp = null;
+		q.add(root);
+		s.push(root);
+		while(!q.isEmpty()) {
+			temp = q.poll();
+			if(temp.getLeft() != null) {
+				q.add(temp.getLeft());
+				s.push(temp.getLeft());
+			}
+			if(temp.getRight() != null) {
+				q.add(temp.getRight());
+				s.push(temp.getRight());
+			}
+		}
+		while(!s.isEmpty()) {
+			temp = s.pop();
+			System.out.println(temp.getData());
+		}
+	}
+	
+	public void reversePath(Node n, int data) {
+		Map<Integer, Integer> temp = new HashMap<>();
+		int pos =0;
+		int level =0;
+		reverseTreePath(n, data, temp, level, pos);
+	}
+	
+	private Node reverseTreePath(Node n, int data, Map<Integer, Integer> temp, int level, int pos) {
+		if(n == null)
+			return null;
+		
+		if(data == n.getData()) {
+			temp.put(level, n.getData());
+			n.setData(temp.get(pos));
+			pos++;
+			return n;
+		}
+		
+		temp.put(level, n.getData());
+		Node left, right = null;
+		
+		left = reverseTreePath(n.getLeft(), data, temp, level+1, pos);
+		
+		if(left == null)
+			right = reverseTreePath(n.getRight(), data, temp, level+1, pos);
+		
+		if(left != null || right != null) {
+			n.setData(temp.get(pos));
+			pos++;
+			return(left != null ? left: right);
+		}
+		
+		return null;
+		
+	}
+	
+	//Specific Level Order traversal
+	public void specificLevelOrder(Node n) {
+		if(n == null)
+			return;
+		System.out.print(n.getData()+" ");
+		if(n.getLeft() != null)
+			System.out.print(n.getLeft().getData()+" "+n.getRight().getData()+" ");
+		
+		if(n.getLeft().getLeft() == null)
+			return;
+		
+		Queue<Node> q =new LinkedList<Node>();
+		q.add(n.getLeft());
+		q.add(n.getRight());
+		Node first, sec = null;
+		while(!q.isEmpty()) {
+			first = q.poll();
+			sec = q.poll();
+			
+			System.out.print(first.getLeft().getData()+" "+sec.getRight().getData()+" ");
+			System.out.print(first.getRight().getData()+" "+sec.getLeft().getData()+" ");
+			
+			if(first.getLeft().getLeft() != null) {
+				q.add(first.getLeft());
+				q.add(sec.getRight());
+				q.add(first.getRight());
+				q.add(sec.getLeft());
+			}
+		}
+	}
+	
+	//lowest common ancestor
+	public Node lca(Node root, int n1, int n2) {
+		if(root == null)
+			return null;
+		
+		if(root.getData() == n1 || root.getData() ==n2)
+			return root;
+		
+		Node left = lca(root.getLeft(), n1, n2);
+		Node right = lca(root.getRight(), n1, n2);
+		
+		if(left != null && right != null)
+			return root;
+		
+		if(left == null && right == null)
+			return null;
+		
+		return left != null ? left : right;
+	}
+	
+	//finding Distance between root and given node
+	
+	
+	
+
+}
