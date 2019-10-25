a6a53a731c3ea35c16c59a8ae2270c984b3366fe
mephi007 <roysumit949@gmail.com>
2019-10-25 14:32:32 +0530

    Pascal Triangle is nothing but Binomial Theorem

---
+++ ArraysAlgo/src/leetCodeEasy/PascalTriangle.java
@@ -0,0 +1,45 @@
+package leetCodeEasy;
+
+import java.util.*;
+
+public class PascalTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		System.out.println(pascalTrianle(n));
+
+	}
+
+	private static List<List<Integer>> pascalTrianle(int n) {
+		// TODO Auto-generated method stub
+		List<List<Integer>> res= new ArrayList<>();
+		helper(res,n);
+		return res;
+	}
+
+	private static void helper(List<List<Integer>> res, int n) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<n; i++) {
+			List<Integer> arr = new ArrayList<>();
+			for(int j =0; j<=i; j++) {
+				if(j==0 || i==0 || i==j)
+					arr.add(1);
+				else {
+					int value = fact(i)/ (fact(j)*fact(i-j));
+					arr.add(value);
+				}
+			}
+			res.add(arr);
+		}
+		
+	}
+
+	private static int fact(int i) {
+		// TODO Auto-generated method stub
+		if(i == 0)
+			return 1;
+		return i*fact(i-1);
+	}
+
+}
